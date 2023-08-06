71cc4d22a42fd688ccf0786864899f5a57f05da1
Arturo Zepeda <aaze92@gmail.com>
2023-08-05 22:02:17 -0600

    añado ajustes para que se mantenga de manera local la información del carrito

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted, watch } from 'vue';
 import {db} from './data/guitarras.js';
 import Guitarra from './components/Guitarra.vue';
 import Header from './components/Header.vue';
@@ -8,10 +8,26 @@ import Footer from './components/Footer.vue';
 const guitarras = ref([]);
 const carrito = ref([]);
 const guitarra = ref({});
+
+watch(carrito,()=>{
+    guardarLocalStorage();
+},{
+    deep:true
+});
+
 onMounted(() => {
   guitarras.value = db;
   guitarra.value = db[3];
+  const carritoStorage = localStorage.getItem('carrito');
+  if (carritoStorage) {
+    carrito.value = JSON.parse(carritoStorage);
+  }
 })
+
+const guardarLocalStorage =()=>{
+    localStorage.setItem('carrito',JSON.stringify(carrito.value));
+}
+
 const agregarCarrito =(guitarra)=>{
     const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id);
       if (existeCarrito >=0) {
@@ -21,23 +37,28 @@ const agregarCarrito =(guitarra)=>{
         carrito.value.push(guitarra);
       }  
 }
+
 const decrementaCantidad =(id)=>{
     const decrementaCarrito = carrito.value.findIndex(producto => producto.id === id);
     if (carrito.value[decrementaCarrito].cantidad > 1) {
         carrito.value[decrementaCarrito].cantidad--;
     }
 }
+
 const incrementaCantidad =(id)=>{
     const incrementaCarrito = carrito.value.findIndex(producto => producto.id === id);
     carrito.value[incrementaCarrito].cantidad++;
 }
+
 const eliminaCarrito =(id)=>{
     const eliminaCarrito = carrito.value.findIndex(producto => producto.id === id);
     carrito.value.splice(eliminaCarrito,1);
 }
+
 const vaciarCarrito =()=>{
     carrito.value = [];
 }
+
 </script>
 
 <template>
